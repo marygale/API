634e37675c1c3c71502986b3f921ef6027adb3bc
Gale <ladyemjay043@gmail.com>
2018-05-13 21:40:01 +0800

    create user

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
+error_reporting(E_ALL);
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 define( "DB_DSN", 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"] );
 define( "DB_HOST", $dbopts["host"]);
 define( "DB_USERNAME", $dbopts["user"] );
 define( "DB_PASSWORD", $dbopts["pass"]);
+
+?>
--- dump.sql
+++ dump.sql
@@ -33,6 +33,6 @@ CREATE TABLE users (
   is_active integer NOT NULL DEFAULT '1'
 );
 
-
-
+/*postgres://fccczroxijlspx:b5d3146f1ff4b565a3c72f2ec3aa7dfade6aae3b4819d597b9e863b540e93c96@ec2-54-83-1-94.compute-1.amazonaws.com:5432/d9qqrpvoldsupj
+{ ["scheme"]=> string(8) "postgres" ["host"]=> string(38) "ec2-54-83-1-94.compute-1.amazonaws.com" ["port"]=> int(5432) ["user"]=> string(14) "fccczroxijlspx" ["pass"]=> string(64) "b5d3146f1ff4b565a3c72f2ec3aa7dfade6aae3b4819d597b9e863b540e93c96" ["path"]=> string(15) "/d9qqrpvoldsupj" }*/
 
