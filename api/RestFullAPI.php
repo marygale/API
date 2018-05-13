839b7ad910840ed3d11ac088ec3439c34271bfc0
Gale <ladyemjay043@gmail.com>
2018-05-14 01:28:03 +0800

    add api

--- api/RestFullAPI.php
+++ api/RestFullAPI.php
@@ -1,79 +1,11 @@
 <?php
+echo 'test';
 include_once('../config.php');
 
 require_once 'utils/API.php';
 error_reporting(E_ALL ^ E_STRICT);
 
-class RestFullAPI extends API{
-    protected $oToken  = null;
-    protected $iCurrentUserId = 0;
-    protected $con = null;
 
-    const TTT_VERSION = 'v1';
-
-    public function __construct()
-    {
-        echo 'galecons';
-        $this->con = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-        $this->con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: *");
-        header("Access-Control-Request-Headers: X-Requested-With, accept, content-type");
-        header("Access-Control-Allow-Headers:Content-Type, Accept, Authorization, X-Requested-With");
-        header('Cache-Control: no-cache, must-revalidate');
-        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-        header("Content-Type: application/json");
-
-    }
-
-    protected function getUsers(){
-        if($this->method('GET')){
-            $sql = "Select * FROM users";
-            $query = $this->con->prepare( $sql );
-            $query->execute();
-            $results = $query->fetchAll(PDO::FETCH_ASSOC);
-            return $results;
-        }
-    }
-
-    public function test(){ return 'test';
-        if($this->method('POST')){
-            return 'this is a test';
-        }
-    }
-    /*public function __construct($request, $origin) {
-        parent::__construct ( $request );
-        if ($this->verb != '') {
-            error_log("Checking token --> " . $this->verb);
-            $this->oToken = UserTokensHandler::getByToken( $this->verb );
-            if ($this->oToken != null) {
-                $this->iCurrentUserId = $this->oToken->getUserId();
-                error_log("Token is valid!!! -> UserId is " . $this->iCurrentUserId);
-            }
-        }
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: *");
-        header("Access-Control-Request-Headers: X-Requested-With, accept, content-type");
-        header("Access-Control-Allow-Headers:Content-Type, Accept, Authorization, X-Requested-With");
-        header('Cache-Control: no-cache, must-revalidate');
-        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-        header("Content-Type: application/json");
-    }*/
-
-}
-
-// Requests from the same server don't have a HTTP_ORIGIN header
-if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
-    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
-}
-
-try {
-    $API = new RestFullAPI ($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
-    echo $API->processAPI();
-} catch (Exception $e) {
-    echo json_encode(array('error' => $e->getMessage()));
-}
 
 
 
