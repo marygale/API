6db5dfcc26d7928dd324c623df59f45f8a4fd34b
Gale <ladyemjay043@gmail.com>
2018-05-13 21:57:18 +0800

    debug

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo 'gale';
+include_once('config.php');
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 define( "DB_DSN", 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"] );
@@ -9,4 +10,4 @@ define( "DB_PASSWORD", $dbopts["pass"]);
 
 var_dump($dbopts["host"]);
 echo 'Password : ' .DB_PASSWORD; echo '<br/>';
-echo 'DB_USERNAME : ' .DB_USERNAME
+echo 'DB_USERNAME : ' .DB_USERNAME;
