1b960bbe60874b36aaf7aaa478decf78d5e5b250
Gale <ladyemjay043@gmail.com>
2018-05-13 18:17:34 +0800

    initial

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 echo 'gale';
-$dbopts = parse_url(getenv('DATABASE_URL'));
-var_dump($dbopts);die;
 
-/*$dbopts = parse_url(getenv('DATABASE_URL'));
+$dbopts = parse_url(getenv('DATABASE_URL'));
 define( "DB_DSN", 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"] );
-define( "DB_HOST", $dbopts["host"] );
+define( "DB_HOST", $dbopts["host"]);
 define( "DB_USERNAME", $dbopts["user"] );
 define( "DB_PASSWORD", $dbopts["pass"]);
 
-
+var_dump($dbopts["host"]);
 echo 'Password : ' .DB_PASSWORD; echo '<br/>';
-echo 'DB_USERNAME : ' .DB_USERNAME*/
+echo 'DB_USERNAME : ' .DB_USERNAME
