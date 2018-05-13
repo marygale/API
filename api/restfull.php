ab85b6fecb33875b2b94706b03ad4e29bf469d87
Gale <ladyemjay043@gmail.com>
2018-05-14 00:23:21 +0800

    test

--- api/restfull.php
+++ api/restfull.php
@@ -13,6 +13,7 @@ class restfull extends API{
 
     public function __construct()
     {
+        return 'gale';
         $this->con = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
         $this->con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
