dd63eb5073a63cf01f89a75263d36e5394459fbb
Gale <ladyemjay043@gmail.com>
2018-05-13 23:54:38 +0800

    add htacces

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-echo 'gale';
+
 $dbopts = parse_url(getenv('DATABASE_URL'));
 define( "DB_DSN", 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"] );
 define( "DB_HOST", $dbopts["host"]);
