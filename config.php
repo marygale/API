0ae270673f72fb13f2bbc0fd54664060b03ca7a0
Gale <ladyemjay043@gmail.com>
2018-05-13 21:44:10 +0800

    add config

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL);
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-define( "DB_DSN", 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"] );
+define( "DB_DSN", 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"] );echo 'gale';die;
 define( "DB_HOST", $dbopts["host"]);
 define( "DB_USERNAME", $dbopts["user"] );
 define( "DB_PASSWORD", $dbopts["pass"]);
-
-?>
+/*
+?>*/
