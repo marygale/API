6cc400fca064acf8c02a9998ab22fe911778b938
Gale <ladyemjay043@gmail.com>
2018-05-14 15:35:55 +0800

    htaccess

--- api/RestFullAPI.php
+++ api/RestFullAPI.php
@@ -4,18 +4,8 @@ include_once('../config.php');
 require_once '../utils/API.php';
 error_reporting(E_ALL ^ E_STRICT);
 
-if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
-    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
-}
 
-try {
-    var_dump($_REQUEST['request']);var_dump($_SERVER['HTTP_ORIGIN']);die;
-    $API = new RestFullAPI ($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
-    echo $API->processAPI();
-} catch (Exception $e) {echo 'catch ';die;
-    echo json_encode(array('error' => $e->getMessage()));
-}
-/*class RestFullAPI extends API{
+class RestFullAPI extends API{
 
     protected $oToken         = null;
     protected $iCurrentUserId = 0;
@@ -46,9 +36,10 @@ try {
         }
     }
 
-    protected function test(){ return 'test pbf';
+    protected function test(){
         if($this->method('POST')){
-            return 'this is a test';
+            $res = 'this is a test';
+            return $res;
         }
     }
 
@@ -65,7 +56,7 @@ try {
     echo $API->processAPI();
 } catch (Exception $e) {echo 'catch ';die;
     echo json_encode(array('error' => $e->getMessage()));
-}*/
+}
 
 
 
