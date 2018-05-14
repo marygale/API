47b6000ba83da1b4b7e2bb2fbfbbf8f438719cb5
Gale <ladyemjay043@gmail.com>
2018-05-14 15:19:29 +0800

    htaccess

--- api/RestFullAPI.php
+++ api/RestFullAPI.php
@@ -1,11 +1,11 @@
 <?php
-
+var_dump($_REQUEST['request']);var_dump($_SERVER['HTTP_ORIGIN']);die;
 if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
 }
 
 try {
-    $API = new RestFullAPI ($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);var_dump($_REQUEST['request']);var_dump($_SERVER['HTTP_ORIGIN']);die;
+    $API = new RestFullAPI ($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
     echo $API->processAPI();
 } catch (Exception $e) {echo 'catch ';die;
     echo json_encode(array('error' => $e->getMessage()));
