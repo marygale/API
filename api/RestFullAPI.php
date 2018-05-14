175777f9efa5ce98c24fb7b9d025402dda9c3f68
Gale <ladyemjay043@gmail.com>
2018-05-14 15:21:52 +0800

    htaccess

--- api/RestFullAPI.php
+++ api/RestFullAPI.php
@@ -1,20 +1,21 @@
 <?php
-var_dump($_REQUEST['request']);var_dump($_SERVER['HTTP_ORIGIN']);die;
+include_once('../config.php');
+
+require_once 'utils/API.php';
+error_reporting(E_ALL ^ E_STRICT);
+
 if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
 }
 
 try {
+    var_dump($_REQUEST['request']);var_dump($_SERVER['HTTP_ORIGIN']);die;
     $API = new RestFullAPI ($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
     echo $API->processAPI();
 } catch (Exception $e) {echo 'catch ';die;
     echo json_encode(array('error' => $e->getMessage()));
 }
 
-include_once('../config.php');
-
-require_once 'utils/API.php';
-error_reporting(E_ALL ^ E_STRICT);
 
 class RestFullAPI extends API{
 
