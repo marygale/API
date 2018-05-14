0625b448d17403d46fe3063077613a7ff81611f8
Gale <ladyemjay043@gmail.com>
2018-05-14 15:25:03 +0800

    htaccess

--- api/RestFullAPI.php
+++ api/RestFullAPI.php
@@ -4,19 +4,6 @@ include_once('../config.php');
 require_once 'utils/API.php';
 error_reporting(E_ALL ^ E_STRICT);
 
-if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
-    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
-}
-
-try {
-    var_dump($_REQUEST['request']);var_dump($_SERVER['HTTP_ORIGIN']);die;
-    $API = new RestFullAPI ($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
-    echo $API->processAPI();
-} catch (Exception $e) {echo 'catch ';die;
-    echo json_encode(array('error' => $e->getMessage()));
-}
-
-
 class RestFullAPI extends API{
 
     protected $oToken         = null;
@@ -48,7 +35,7 @@ class RestFullAPI extends API{
         }
     }
 
-    public function test(){ return 'test pbf';
+    protected function test(){ return 'test pbf';
         if($this->method('POST')){
             return 'this is a test';
         }
@@ -57,6 +44,17 @@ class RestFullAPI extends API{
 }
 
 
+if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+}
+
+try {
+    var_dump($_REQUEST['request']);var_dump($_SERVER['HTTP_ORIGIN']);die;
+    $API = new RestFullAPI ($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    echo $API->processAPI();
+} catch (Exception $e) {echo 'catch ';die;
+    echo json_encode(array('error' => $e->getMessage()));
+}
 
 
 
