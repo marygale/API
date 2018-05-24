b8d0416558557180a6bcdbb75f9d00cf038a4dee
Gale <ladyemjay043@gmail.com>
2018-05-24 21:01:04 +0800

    dimension

--- api/RestFullAPI.php
+++ api/RestFullAPI.php
@@ -126,7 +126,7 @@ class RestFullAPI extends API{
     public function getQuestionsByDimension($dimensions){
         $dim = [];
         foreach ($dimensions as $key=>$val){
-            array_push($dim, $key);
+            array_push($dim, $val);
         }return var_dump($dim);
         /*$ar =array_map('intval', explode(',', $dim));
         $dim = implode("','",$dim);
